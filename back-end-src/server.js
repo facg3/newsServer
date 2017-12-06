6ca17b154613ed3878fe03cdea9e993bddb9185d
ismail2009 <somaaa2009@live.com>
2017-12-06 18:15:36 +0200

    add pakage-lock

--- back-end-src/server.js
+++ back-end-src/server.js
@@ -2,8 +2,8 @@ const http = require('http');
 const router = require('./router.js');
 
 const server = http.createServer(router);
-const hostname = process.envHOSTNAME || 'localhost';
+const hostname = process.env.HOST || 'localhost';
 const port = process.env.PORT || 8080;
 server.listen(port, hostname, () => {
-console.log(`the server work on port :hostname ${hostname} port : ${port}`);
+  console.log(`the server work on port :hostname ${hostname} port : ${port}`);
 });

6a55a5c151424b6f93784b4ac6e62202ae2e09bf
ismail2009 <somaaa2009@live.com>
2017-12-07 10:06:50 +0200

    fix port

--- back-end-src/server.js
+++ back-end-src/server.js
@@ -3,7 +3,5 @@ const router = require('./router.js');
 
 const server = http.createServer(router);
 const hostname = process.env.HOST || 'localhost';
-const port = process.env.PORT || 5000;
-server.listen(port, () => {
-  console.log(`the server work on port :hostname ${hostname} port : ${port}`);
-});
+const port = process.env.PORT || 8080;
+server.listen(port);

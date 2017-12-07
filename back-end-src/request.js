e892fb4f51afa25ef024fad0cbe51b2b81daf48a
ismail2009 <somaaa2009@live.com>
2017-12-07 10:10:58 +0200

    set console.log

--- back-end-src/request.js
+++ back-end-src/request.js
@@ -6,29 +6,28 @@ const myRequest = (url, response) => {
     method: 'GET',
   };
   request(option, (err, res) => {
-
     if (err) {
       res.end('<h1>Request Failed, internal server error occured</h1>');
     } else {
-      var poster = [];
-      var data = JSON.parse(res.body);
-      var arr = data.articles;
+      const poster = [];
+      const data = JSON.parse(res.body);
+      const arr = data.articles;
       arr.forEach((item) => {
-        var obj = {
-          title:item.title,
-          img:item.urlToImage,
-          date:item.publishedAt,
-          description:item.description,
-          url:item.url
+        const obj = {
+          title: item.title,
+          img: item.urlToImage,
+          date: item.publishedAt,
+          description: item.description,
+          url: item.url,
         };
         poster.push(obj);
       });
 
-       response.end(JSON.stringify(poster));
- }
+      response.end(JSON.stringify(poster));
+    }
   });
 };
 
 module.exports = {
-  myRequest
+  myRequest,
 };

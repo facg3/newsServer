7c28e9fc66fad4b1430576e7d3c6b564b0791e8d
sultanassi95 <mrsultanassi@gmail.com>
2017-12-07 10:24:32 +0200

    fixed :) #32

--- back-end-src/handlers.js
+++ back-end-src/handlers.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const request = require('./request.js');
+
 const homePageHandler = (req, res) => {
   fs.readFile(path.join(__dirname, '..', 'public', 'index.html'), (error, file) => {
     if (error) {
@@ -22,35 +23,41 @@ const publicFileHandler = (req, res) => {
     js: 'application/javascript',
     jpg: 'image/jpeg',
     png: 'image/png',
-    ico : 'image/x-icon'
+    ico: 'image/x-icon',
   };
   fs.readFile(path.join(__dirname, '..', endpoint), (err, file) => {
     if (err) {
       res.writeHead(404, 'Content-Type: text/html');
       res.end('<h1>Page Not Found.</h1>');
     } else {
-      res.writeHead(200, 'Content-type:' + fileType[type]);
+      res.writeHead(200, `Content-type:${fileType[type]}`);
       res.end(file);
     }
   });
 };
 
 const publicNewsHandler = (req, res) => {
-res.writeHead(200,{'Content-type':'application/json'});
-request.myRequest('https://newsapi.org/v2/everything?sources=al-jazeera-english&apiKey=7adfe861e4094bbdb3a274e9dc61d885',
-res);
+  res.writeHead(200, { 'Content-type': 'application/json' });
+  request.myRequest(
+    'https://newsapi.org/v2/everything?sources=al-jazeera-english&apiKey=7adfe861e4094bbdb3a274e9dc61d885',
+    res,
+  );
 };
 
 const sportNewsHandler = (req, res) => {
-res.writeHead(200,{'Content-type':'application/json'})
-request.myRequest('https://newsapi.org/v2/top-headlines?sources=football-italia&apiKey=7adfe861e4094bbdb3a274e9dc61d885',
-res);
+  res.writeHead(200, { 'Content-type': 'application/json' });
+  request.myRequest(
+    'https://newsapi.org/v2/top-headlines?sources=football-italia&apiKey=7adfe861e4094bbdb3a274e9dc61d885',
+    res,
+  );
 };
 
 const musicNewsHandler = (req, res) => {
-res.writeHead(200,{'Content-type':'application/json'});
-request.myRequest('https://newsapi.org/v2/everything?sources=mtv-news&apiKey=7adfe861e4094bbdb3a274e9dc61d885',
-res);
+  res.writeHead(200, { 'Content-type': 'application/json' });
+  request.myRequest(
+    'https://newsapi.org/v2/everything?sources=mtv-news&apiKey=7adfe861e4094bbdb3a274e9dc61d885',
+    res,
+  );
 };
 
 module.exports = {
@@ -58,5 +65,5 @@ module.exports = {
   publicFileHandler,
   publicNewsHandler,
   sportNewsHandler,
-  musicNewsHandler
+  musicNewsHandler,
 };
